56386afb68a5fcfce754e217e6311914e649cd66
Ppojin <25076967+Ppojin@users.noreply.github.com>
2020-02-04 15:41:51 +0900

    state

--- react/sample/sample_1/my-app/src/App.js
+++ react/sample/sample_1/my-app/src/App.js
@@ -1,24 +1,14 @@
 //import
 import React, { Component } from 'react';
-import { Fragment } from 'react';
-// import './App.css'
-import MyIntro from './MyIntro';
-import MyIntro2 from './MyIntro2';
+// import { Fragment } from 'react';
+import Counter from './Counter';
 
 //class
 // render 함수에서는 단 하나의 태그만 return 해야한다.
 class App extends Component {
   render(){
-    const myCard = {
-      name: "Hyojin",
-      email: "im@ppojin.com",
-      phone: "010-1234-5627",
-    }
     return(
-      <Fragment>
-        <MyIntro card={myCard}/>
-        <MyIntro2 card={myCard}/>
-      </Fragment>
+      <Counter/>
     );  
   };
 };
--- react/sample/sample_1/my-app/src/counter.js
+++ react/sample/sample_1/my-app/src/counter.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 class Counter extends Component {
+    state = {
+        count: 100
+    };
+
+    handleAdd = ()=>{
+        this.setState({
+            count: this.state.count + 1
+        });
+    };
+    
+    handleDis = ()=>{
+        this.setState({
+            count: this.state.count - 1 
+        });
+    };
+
     render() {
-        let state = {
-            couter = 0
-        };
 
         return (
             <div>
                 <h1>Counter</h1>
-                <h1 id="num"></h1>
-                <button id="add">+</button>
-                <button id="dis">+</button>
+                <h1 id="num">{this.state.count}</h1>
+                <button onClick={this.handleAdd} id="add">+</button>
+                <button onClick={this.handleDis} id="dis">-</button>
             </div>
         );
     };
