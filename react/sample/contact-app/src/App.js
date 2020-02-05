08416cf2097d843eee9832a2486849eef8c9f4d0
Ppojin <25076967+Ppojin@users.noreply.github.com>
2020-02-05 11:26:13 +0900

    부모에서 출력하기

--- react/sample/contact-app/src/App.js
+++ react/sample/contact-app/src/App.js
@@ -1,15 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 import PhoneForm from './components/phone_form';
 
 class App extends Component{
+  id = 1;
+  state = {
+    contacts: [
+      {
+        id: 0,
+        name: '관리자',
+        phone: '010-1234-5678'
+      }
+    ]
+  }
+
   handleCreate = (data) => {
     console.log(data);
+    const { contacts } = this.state;
+    this.setState({
+      contacts: contacts.concat({
+        id: this.id++,
+        ...data
+      })
+    })
+
+    // this.setState({
+    //   contacts: [
+    //     ...this.state.contacts, 
+    //     {
+    //       id:this.id++,
+    //       ...data
+    //     }
+    //   ]
+    // })
   }
 
   render() {
+    const { contacts } = this.state;
     return(
-      <PhoneForm onCreate={this.handleCreate} />
+      <Fragment>
+        <PhoneForm onCreate={this.handleCreate} />
+        {JSON.stringify(contacts)}
+      </Fragment>
     );
   };
 };
--- react/sample/contact-app/src/components/phone_form.js
+++ react/sample/contact-app/src/components/phone_form.js
@@ -15,30 +15,24 @@ class PhoneForm extends Component {
     handleSubmit = (e) => {
         e.preventDefault();
         this.props.onCreate(this.state);
+        this.setState({ name: '' , phone: '' });
     }
 
     render() {
         return(
             <form onSubmit={this.handleSubmit}>
                 <input 
+                    value={this.state.name}
                     name='name'
                     onChange={this.handleChange} 
                     placeholder='이름을 입력하세요'    
                 />
                 <input 
+                    value={this.state.phone}
                     name='phone'
                     onChange={this.handleChange} 
                     placeholder='전화번호를 입력하세요'  
                 />
-                <div>
-                    <span id='name'>
-                        {this.state.name}
-                    </span>
-                    <span> / </span>
-                    <span id='phone'>
-                        {this.state.phone}
-                    </span>
-                </div>
                 <button type='submit'> 등록 </button>
             </form>
         )
