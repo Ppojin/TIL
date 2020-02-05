3c45f01a49cff272393c6df13158de7e1866a0f4
Ppojin <25076967+Ppojin@users.noreply.github.com>
2020-02-05 13:15:42 +0900

    -

--- react/sample/contact-app/src/components/phone_list.js
+++ react/sample/contact-app/src/components/phone_list.js
@@ -4,13 +4,21 @@ class PhoneList extends Component {
     render(){
         const { data } = this.props; // contacts => data.contacts;
         const trList = data.map((value, index)=>{return (
-            <tr key={value.id}><td>{value.id}</td><td>{value.name}</td><td>{value.phone}</td></tr>
+            <tr>
+                <td>{value.id}</td>
+                <td>{value.name}</td>
+                <td>{value.phone}</td>
+            </tr>
         )})
         return(
             <div>
                 <table border='1'>
                     <tbody>
-                        <tr><td>id</td><td>name</td><td>phone</td></tr>
+                        <tr>
+                            <td>id</td>
+                            <td>name</td>
+                            <td>phone</td>
+                        </tr>
                         {trList}
                     </tbody>
                 </table>
